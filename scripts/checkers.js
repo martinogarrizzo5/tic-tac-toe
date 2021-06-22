d69c3a1c48136c29854be64065a75091649f8d38
Martin <martinmeneghetti@outlook.it>
2021-06-22 17:12:03 +0200

    day 2: main functions finished

---
+++ scripts/checkers.js
@@ -0,0 +1,62 @@
+"use strict";
+// check single column
+function checkColumn(grid, colIndex, sign) {
+    for (var i = 0; i < grid.length; i++) {
+        if (grid[i][colIndex] !== sign) {
+            return false;
+        }
+    }
+    return true;
+}
+// check single row
+function checkRow(grid, rowIndex, sign) {
+    for (var i = 0; i < grid.length; i++) {
+        if (grid[rowIndex][i] !== sign) {
+            return false;
+        }
+    }
+    return true;
+}
+// check right diagonal
+function checkRightDiagonal(grid, sign) {
+    var i = 0;
+    var j = grid.length - 1;
+    while (i < grid.length && j >= 0) {
+        if (grid[i][j] !== sign) {
+            return false;
+        }
+        i += 1;
+        j -= 1;
+    }
+    return true;
+}
+// check left diagonal
+function checkLeftDiagonal(grid, sign) {
+    var i = 0;
+    var j = 0;
+    while (i < grid.length && j < grid.length) {
+        if (grid[i][j] !== sign) {
+            return false;
+        }
+        i += 1;
+        j += 1;
+    }
+    return true;
+}
+// check all diagonals
+function checkDiagonals(grid, sign) {
+    return checkLeftDiagonal(grid, sign) || checkRightDiagonal(grid, sign);
+}
+// check all rows and columns
+function checkRowsAndColumns(grid, sign) {
+    for (var i = 0; i < grid.length; i++) {
+        if (checkColumn(grid, i, sign) || checkRow(grid, i, sign)) {
+            return true;
+        }
+    }
+    return false;
+}
+// check entire grid
+function checkGrid(grid, sign) {
+    return checkRowsAndColumns(grid, sign) || checkDiagonals(grid, sign);
+}
--- tic-tac-toe.ts
+++ tic-tac-toe.ts
@@ -1,57 +1,78 @@
-"use strict";
-var grid = [
+const grid = [
     ["", "", ""],
     ["", "", ""],
     ["", "", ""],
 ];
-var freeCells = grid.length * grid.length; // number of free cells
-var isGameOver = false;
-var gridHTML = '<div class="grid"><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div></div>';
-var buttonHTML = '<button class="btn" id="game-btn">Challenge The Death</button>';
-var gridCells = document.getElementsByClassName("grid__col"); // get grid cells reference
-var container = document.getElementById("game-container");
+let freeCells = grid.length * grid.length; // number of free cells
+let isGameOver: boolean = false;
+
+const gridHTML =
+    '<div class="grid"><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div><div class="grid__col"></div></div>';
+const buttonHTML =
+    '<button class="btn" id="game-btn">Challenge The Death</button>';
+
+const gridCells = document.getElementsByClassName("grid__col"); // get grid cells reference
+const container = document.getElementById("game-container");
+
 // start with the button on the screen
-container.innerHTML = buttonHTML;
-var button = document.getElementById("game-btn");
-button === null || button === void 0 ? void 0 : button.addEventListener("click", function () {
+container!.innerHTML = buttonHTML;
+const button = document.getElementById("game-btn");
+
+button?.addEventListener("click", () => {
     startGame();
 });
+
 function startGame() {
-    container.innerHTML = gridHTML;
-    var _loop_1 = function (i) {
+    container!.innerHTML = gridHTML;
+
+    // add click listener to each grid cell
+    for (let i = 0; i < gridCells.length; i++) {
         gridCells[i].addEventListener("click", function () {
             if (!isGameOver) {
                 //////////////// player move
                 // update js grid
-                var rowIndex = Math.floor(i / 3);
-                var colIndex = i % 3;
+                const rowIndex = Math.floor(i / 3);
+                const colIndex = i % 3;
+
                 if (grid[rowIndex][colIndex] == "") {
                     grid[rowIndex][colIndex] = "X";
                     freeCells--;
+
                     // update html grid adding sign to cell when clicked
                     gridCells[i].innerHTML = '<div class="cross"></div>';
-                    var isXWinner = checkGrid(grid, "X");
+
+                    const isXWinner = checkGrid(grid, "X");
+
                     if (isXWinner) {
                         console.log("X is the Winner");
                         isGameOver = true;
                         endGame();
                         return;
                     }
+
                     ////////////// computer move
-                    var randomRowIndex = void 0;
-                    var randomColIndex = void 0;
+                    let randomRowIndex;
+                    let randomColIndex;
+
                     if (freeCells >= 1) {
                         do {
                             randomRowIndex = Math.floor(Math.random() * 3); // random number between 0 and 2
                             randomColIndex = Math.floor(Math.random() * 3);
                         } while (grid[randomRowIndex][randomColIndex] !== "");
+
                         grid[randomRowIndex][randomColIndex] = "O";
                         freeCells--;
+
                         // update html grid adding sign to cell when clicked
-                        gridCells[randomRowIndex * 3 + randomColIndex].innerHTML = '<div class="circle"></div>';
+                        gridCells[
+                            randomRowIndex * 3 + randomColIndex
+                        ].innerHTML = '<div class="circle"></div>';
                     }
+
                     console.table(grid);
-                    var isOWinner = checkGrid(grid, "O");
+
+                    const isOWinner = checkGrid(grid, "O");
+
                     if (isOWinner) {
                         console.log("O is the winner!");
                         isGameOver = true;
@@ -61,16 +82,13 @@ function startGame() {
                 }
             }
         });
-    };
-    // add click listener to each grid cell
-    for (var i = 0; i < gridCells.length; i++) {
-        _loop_1(i);
     }
 }
+
 function endGame() {
     if (isGameOver) {
-        container.innerHTML +=
+        container!.innerHTML +=
             '<div class="game-over"><h3 class="title">Hai vinto</h3><button id="play-again-button">Play Again</button></div>';
-        var playAgainButton = document.getElementById("play-again-button");
+        const playAgainButton = document.getElementById("play-again-button");
     }
 }
